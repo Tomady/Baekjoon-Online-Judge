735fe15d8ed1006bbb2c92a43839b6c6391ffcb5
Tomady <87797173+Tomady@users.noreply.github.com>
2022-07-12 00:10:46 +0900

    11654

---
+++ baekjoon/src/bronze/Baekjoon11654.java
@@ -0,0 +1,19 @@
+package bronze;
+
+import java.util.Scanner;
+
+public class Baekjoon11654 {
+
+	public static void main(String[] args) {
+		// bronze
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+		char ch = str.charAt(0);
+		int ascii = (int)ch;
+		
+		System.out.println(ascii);
+		
+		sc.close();
+
+	}
+}
